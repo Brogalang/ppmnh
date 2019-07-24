57ac118e0dcd6c26ab154f81b4833b28b4e7c092
Rashomon <mohammad18018@mail.unpad.ac.id>
2019-07-25 00:26:46 +0700

    front

--- resources/views/front/awal/santri.blade.php
+++ resources/views/front/awal/santri.blade.php
@@ -5,7 +5,7 @@
       
       
 
-      <div class="site-blocks-cover" style="background-image: url(images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="site-blocks-cover" style="background-image: url(assets/front/images/IMG_1397.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
         <div class="container">
           <div class="row align-items-center justify-content-center">
             <div class="col-md-7 text-center" data-aos="fade">
